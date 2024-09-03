5643e19fa272a6038ce77feef66e9ba822d01e5d
cindyhan <cindyhan@tencent.com>
2024-09-03 23:26:05 +0800

    add 485.py

---
+++ leetcode_test/485.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python2.7
+# -*- coding: utf8 -*-
+
+"""
+485.py
+author: cindyhan 2024/9/3
+
+"""
+
+
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        result = 0
+        for i in nums:
+            if i == 1:
+                count +=1
+                if result < count:
+                    result = count
+            else:
+                count = 0
+
+
+        return result
+
+
+obj = Solution()
+nums1 = [1, 1, 0, 1, 1, 1]
+nums2 = [1, 0, 1, 1, 0, 1]
+res1 = obj.findMaxConsecutiveOnes(nums1)
+res2 = obj.findMaxConsecutiveOnes(nums2)
+print(res1, res2)
